9bcc095161c0512e606a6bced1543fb0285c08d5
teroz <terence.namusonge@gmail.com>
2020-07-30 13:23:03 +0200

    added actual versioned deb

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
-
-useradd --system tlog
-chown tlog:tlog /usr/local/bin/tlog-rec-session
-chown tlog:tlog /run/tlog
-chmod g+s /usr/local/bin/tlog-rec-session
-chmod u+s /usr/local/bin/tlog-rec-session
+USER=tlog
+if ! getent passwd $USER > /dev/null 2>&1; then
+   useradd --system $USER
+fi
+chown $USER:$USER /usr/bin/tlog-rec-session /run/tlog
+chmod g+s /usr/bin/tlog-rec-session
+chmod u+s /usr/bin/tlog-rec-session
+cd /usr/lib/
+ln -s libtlog.so.0.0.0 libtlog.so.0
+ln -s libtlog.so.0.0.0 libtlog.so
+ln -s libtlog.a libtlog.la
+mandb /usr/share/man/man5 > /dev/null
+mandb /usr/share/man/man8 > /dev/null
---
+++ scripts/postremove.sh
@@ -0,0 +1,1 @@
+rm  /usr/lib/libtlog.so.0 /usr/lib/libtlog.so /usr/lib/libtlog.la
